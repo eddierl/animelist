5538a9549322921bb4228ffe7a2f5a8ce75c31aa
Eddie Erlich <edlich6@gmail.com>
2025-10-20 19:45:50 +0800

    feat(apollo): add persist local storage

--- lib/apollo.ts
+++ lib/apollo.ts
@@ -1,8 +1,15 @@
-import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
+import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
+import { persistCache, LocalStorageWrapper } from "apollo3-cache-persist";
+
+const cache = new InMemoryCache();
+await persistCache({
+  cache,
+  storage: new LocalStorageWrapper(window.localStorage),
+});
 
 const client = new ApolloClient({
-  link: new HttpLink({ uri: 'https://graphql.anilist.co' }),
-  cache: new InMemoryCache(),
+  link: new HttpLink({ uri: "https://graphql.anilist.co" }),
+  cache,
 });
 
 export default client;
