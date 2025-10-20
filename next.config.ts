31817d3312ae6f84d9f93272d5464c3ade21f9f3
Eddie Erlich <edlich6@gmail.com>
2025-10-20 19:33:43 +0800

    calculate commit hash to build time

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,16 @@
 import type { NextConfig } from "next";
+import { execSync } from "child_process";
+
+const gitSha = execSync("git rev-parse HEAD", { encoding: "utf8" }).trim();
 
 const nextConfig: NextConfig = {
+  env: {
+    NEXT_PUBLIC_GIT_SHA: gitSha,
+  },
   /* config options here */
-  images:{
-    remotePatterns:[new URL("https://s4.anilist.co/**")]
-  }
+  images: {
+    remotePatterns: [new URL("https://s4.anilist.co/**")],
+  },
 };
 
 export default nextConfig;
